4b60fce90cffa0c8699721733dc8133d333af02f
levani <levancho.begashvili@gmail.com>
2022-07-02 14:06:03 +0400

    create main

---
+++ src/com/leva/Main.java
@@ -0,0 +1,20 @@
+package com.leva;
+
+import static com.leva.Task1.palindrome;
+import static com.leva.Task2.coinSplit;
+import static com.leva.Task3.contains;
+import static com.leva.Task4.properly;
+import static com.leva.Task5.variants;
+
+public class Main {
+
+    public static void main(String[] args) {
+        palindrome();
+        coinSplit();
+        contains();
+        properly();
+        variants();
+
+    }
+
+}
